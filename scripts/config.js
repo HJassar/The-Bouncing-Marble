6e89dd824a6b41d108ec9a460e9098f0771a5434
Hamza Jassar <jassarhamza@gmail.com>
2022-07-25 21:00:30 -0500

    lesson 3 initial setup

--- scripts/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    min: 1,
-    max: 10,
-    step: 0.2,
-}
--- scripts/utils.js
+++
@@ -1,7 +0,0 @@
-import config from "./config.js";
-
-export const setPlayBackRate = (/** @type {any[]} */ axises, /** @type {number} */ rate) => {
-    axises.forEach(axis => {
-        axis.getAnimations()[0].playbackRate = rate;
-    });
-}
